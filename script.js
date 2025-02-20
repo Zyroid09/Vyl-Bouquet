4d25ae39cc1ad7d15145189152fed3d69903be5c
Naufal Abdala Syawali <88916264+Zyroid09@users.noreply.github.com>
2025-02-20 23:34:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -20,3 +20,24 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const navbarHeight = document.querySelector(".navbar").offsetHeight;
+  const links = document.querySelectorAll("a[href^='#']");
+
+  links.forEach((link) => {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      const targetId = this.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+        window.scrollTo({
+          top: targetElement.offsetTop - navbarHeight - 10,
+          behavior: "smooth",
+        });
+      }
+    });
+  });
+});
